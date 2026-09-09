97574a802af67a5d3de7bdd680bbadf5eed5e7f1
leluus <leonamdeziderio38115@gmail.com>
2026-09-09 14:35:29 -0300

    Atualização do site

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,7 +6,7 @@ function Logo() {
     <Link to="/" className="logo">
       <span className="logo__icon">V</span>
       <span className="logo__text">
-        Vitrine <span className="logo__accent">Alegre</span>
+        julia <span className="logo__accent">Alegre</span>
       </span>
     </Link>
   );
